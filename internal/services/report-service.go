c0cae0c2a94c6c389ef86f3d9ac44c4827bfaf47
Henry Bravo <hbravos.info@gmail.com>
2024-08-28 14:28:30 -0500

    test: 💍 perf golang

--- internal/services/report-service.go
+++ internal/services/report-service.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/henrybravo/micro-report/internal/report"
 	repo "github.com/henrybravo/micro-report/internal/repositories"
+	"time"
 )
 
 type ReportService struct {
@@ -37,10 +39,15 @@ func (s *ReportService) CreateSalesPaginated(businessID, period string, isPagina
 	return sales, pagination, nil
 }
 func (s *ReportService) CreateExcelSales(businessID, period string) (*bytes.Buffer, error) {
+	start := time.Now()
 	sales, _, err := s.salesRepo.GetSalesReports(businessID, period, repo.PaginationParams{Pagination: false})
 	if err != nil {
 		return nil, err
 	}
+	duration := time.Since(start)
+	fmt.Printf("Consulta a la base de datos completada en %v\n", duration)
+	fmt.Println("Procediendo a generar el EXCEL")
+
 	excel, err := s.excelGenerator.GenerateSalesReport(sales)
 	if err != nil {
 		return nil, err
@@ -48,12 +55,19 @@ func (s *ReportService) CreateExcelSales(businessID, period string) (*bytes.Buff
 	return excel, nil
 }
 func (s *ReportService) CreatePDFSales(businessID, period string) (*bytes.Buffer, error) {
+	start := time.Now()
 	business, err := s.businessRepo.GetBusinessByID(businessID)
 	sales, _, err := s.salesRepo.GetSalesReports(businessID, period, repo.PaginationParams{Pagination: false})
+	duration := time.Since(start)
+	fmt.Printf("Consulta a la base de datos completada en %v, total: %d items\n", duration, len(sales))
+	fmt.Println("Procediendo a generar el PDF")
+	start = time.Now()
 	if err != nil {
 		return nil, err
 	}
 	pdf, err := s.pdfGenerator.GeneratePDF(*business, sales, period)
+	duration = time.Since(start)
+	fmt.Printf("Generación del PDF completada en %v\n", duration)
 	if err != nil {
 		return nil, err
 	}
