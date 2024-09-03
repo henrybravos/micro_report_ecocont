40d6babc08730f27a10552cef89b6433463317b0
Henry Bravo <hbravos.info@gmail.com>
2024-09-03 09:00:40 -0500

    feat: 🎸 implement "libro de trabajo" report

--- internal/services/journal-service.go
+++ internal/services/journal-service.go
@@ -4,93 +4,44 @@ import (
 	"connectrpc.com/connect"
 	"context"
 	"errors"
-	"github.com/henrybravo/micro-report/internal/report/excel"
-	"github.com/henrybravo/micro-report/internal/report/pdf"
 	repo "github.com/henrybravo/micro-report/internal/repositories"
 	"github.com/henrybravo/micro-report/pkg/validate"
 	v1 "github.com/henrybravo/micro-report/protos/gen/go/v1"
 	"log"
+	"strings"
 )
 
-type SalesServer struct {
-	SalesRepo    *repo.SalesRepository
-	BusinessRepo *repo.BusinessRepository
+type JournalServer struct {
+	JournalRepo *repo.JournalRepository
 }
 
-func (s *SalesServer) RetrieveSalesPaginatedReport(
+func (s *JournalServer) RetrieveJournalPaginatedReport(
 	_ context.Context,
-	req *connect.Request[v1.RetrieveSalesPaginatedReportRequest],
-) (*connect.Response[v1.RetrieveSalesPaginatedReportResponse], error) {
+	req *connect.Request[v1.RetrieveJournalPaginatedReportRequest],
+) (*connect.Response[v1.RetrieveJournalPaginatedReportResponse], error) {
 	log.Println("Request headers: ", req.Header())
 	businessID := req.Msg.GetBusinessId()
 	period := req.Msg.GetPeriod()
-	pageSize := req.Msg.GetPageSize()
-	page := req.Msg.GetPage()
-	paginationDefault := GetPaginationOrDefault(page, pageSize)
+	isConsolidated := req.Msg.GetIsConsolidated()
+	includeClose := req.Msg.GetIncludeClose()
+	includeCuBa := req.Msg.GetIncludeCuBa()
 	if !validate.IsValidUUID(businessID) {
 		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid business ID"))
 	}
 	if !validate.IsValidPeriod(period) {
 		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid period"))
 	}
-
-	sales, pagination, err := s.SalesRepo.GetSalesReports(businessID, period, paginationDefault)
-
+	partsPeriod := strings.Split(period, "-")
+	year := partsPeriod[0]
+	month := partsPeriod[1]
+	journalEntries, err := s.JournalRepo.GetJournalEntries(businessID, year, month, isConsolidated, includeClose, includeCuBa)
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
-	pagination.Page = paginationDefault.Page
-	pagination.PageSize = paginationDefault.PageSize
-	pagination.Offset = paginationDefault.Offset
-	res := connect.NewResponse(&v1.RetrieveSalesPaginatedReportResponse{
-		Sales:      sales,
-		Pagination: pagination,
+
+	res := connect.NewResponse(&v1.RetrieveJournalPaginatedReportResponse{
+		Journals: journalEntries,
 	})
 	res.Header().Set("Report-Version", "v1")
 	return res, nil
-}
-
-func (s *SalesServer) RetrieveSalesResourceReport(
-	_ context.Context,
-	req *connect.Request[v1.RetrieveSalesResourceReportRequest],
-) (*connect.Response[v1.RetrieveSalesResourceReportResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	businessID := req.Msg.GetBusinessId()
-	period := req.Msg.GetPeriod()
-	typeResource := req.Msg.GetType()
-	if !validate.IsValidUUID(businessID) {
-		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid business ID"))
-	}
-	if !validate.IsValidPeriod(period) {
-		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid period"))
-	}
-	if typeResource == v1.TypeResource_TYPE_RESOURCE_UNSPECIFIED {
-		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid type resource"))
-	}
-
-	sales, _, err := s.SalesRepo.GetSalesReports(businessID, period, nil)
-	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-	business, err := s.BusinessRepo.GetBusinessByID(businessID)
-	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-	var pathName string
-	if typeResource == v1.TypeResource_TYPE_RESOURCE_XLSX {
-		generatorXLSX := excel.NewSalesGenerator()
-		pathName, err = generatorXLSX.GenerateSalesReport(sales)
-	}
-	if typeResource == v1.TypeResource_TYPE_RESOURCE_PDF {
-		generatorPdf := pdf.NewSalesGenerator()
-		pathName, err = generatorPdf.GenerateSalesReport(business, sales, period)
-	}
-	if err != nil {
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-	res := connect.NewResponse(&v1.RetrieveSalesResourceReportResponse{
-		Url: pathName,
-	})
-	res.Header().Set("Report-Version", "v1")
-	return res, err
 }
